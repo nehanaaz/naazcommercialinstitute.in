8be9d67cb5598cdce26250a91e3d694cd84fb951
jewel998 <jewel998@users.noreply.github.com>
2026-01-03 10:21:00 +0000

    deploy: b8028665eb1616e74b45c94fef46d3fc8098f1fe

---
+++ _astro/image.BnLhu2o5.js
@@ -0,0 +1,1 @@
+import{j as e}from"./jsx-runtime.D_zvdyIk.js";import"./index.DPV4Kj-g.js";function a({...r}){const o=t=>{t.currentTarget.src="/placeholder.svg"};return e.jsx("img",{...r,onError:o})}export{a as I};
--- _astro/image.DCkSskpp.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime.D_zvdyIk.js";function s({...r}){const o=e=>{e.currentTarget.src="/placeholder.svg"};return t.jsx("img",{...r,onError:o})}export{s as I};
